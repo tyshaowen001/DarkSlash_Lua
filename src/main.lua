492908155049b7801ce0d155f11585dee95db7f8
tyshaowen001 <tyshaowen001@gmail.com>
2017-12-26 19:18:21 +0800

    111

--- src/main.lua
+++ src/main.lua
@@ -17,20 +17,15 @@ local function main()
         if event == "enter" then
             
             local child = myscene:getChildByName ('Canvas'):getChildByName ('menuAnim')      
-            local temp = creatorReader:getAnimationManager ()
-            print (os.date ())
-            local co = coroutine.create (function ()
-                print(11)
-                local lable = cc.Label:createWithSystemFont (os.time ()
-                , "Arial", 20)
-                myscene:addChild (lable)
-                lable:move (display.cx, display.cy + 200)
-            end)
-            coroutine.resume (co)
-
-            print (coroutine.status (co))
-            print (co)
-            coroutine.resume (co)
+            local temp = creatorReader:getAnimationManager ()       
+--            local co = coroutine.create (function ()
+--                print(11)
+--                local lable = cc.Label:createWithSystemFont (os.time ()
+--                , "Arial", 20)
+--                myscene:addChild (lable)
+--                lable:move (display.cx, display.cy + 200)
+--            end)
+--            coroutine.resume (co)
 --            local spschedule = cc.Sprite:create ()
 --            myscene:addChild (spschedule)
 --            spschedule:runAction(cc.Sequence:create(cc.DelayTime:create(4),cc.CallFunc:create(function(args)
@@ -44,7 +39,7 @@ local function main()
             callbackEntry = cc.Director:getInstance():getScheduler():scheduleScriptFunc(function(dt)  
                 temp:playAnimationClip (child, "menuAnim") 
                 cc.Director:getInstance ():getScheduler ():unscheduleScriptEntry (callbackEntry)
-            end, 2.0, false)
+            end, 0.5, false)
 
 
         elseif event == "enterTransitionFinish" then
@@ -61,7 +56,7 @@ local function main()
      for k, v in pairs( getmetatable(myscene:getChildByName('Canvas'))) do
           print(k, v)
      end
-    myscene:getChildByName('Canvas'):setScale(0.2)
+   -- myscene:getChildByName('Canvas'):setScale(0.2)
     cc.Director:getInstance ():replaceScene (myscene)
 end
 
